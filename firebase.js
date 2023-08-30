0f6cfdc1aa9065b6cfa64449d15300f581da3e3b
Mark-2002 <99177147+Mark2-002@users.noreply.github.com>
2023-08-30 23:17:15 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAN3XAQIu2KModOLDYnzcP3I25ghxAV4HI",
+    authDomain: "fir-f737b.firebaseapp.com",
+    projectId: "fir-f737b",
+    storageBucket: "fir-f737b.appspot.com",
+    messagingSenderId: "368992269628",
+    appId: "1:368992269628:web:b942a7e8bef13cde101a39",
+    measurementId: "G-FNFB9F0R8R"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
